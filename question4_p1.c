87e0e41533dba9b7bbba271a8d35f5e00f35ee5d
akshavibaskaran <akshavi.bt@gmail.com>
2025-11-06 18:20:25 -0500

    Added question 4 process 1 and 2

---
+++ question4_p1.c
@@ -0,0 +1,73 @@
+/*
+* QUESTION 4 - Sharing memory between parent and child processes
+* Using the memory mapping functions to share memory between a parent and child process.
+*
+* question4_p1.c: Creates shared memory and spawns question4_p2
+*/
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/mman.h> /* for memory mapping functions */
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+
+typedef struct { /* struct for the multiple variable and counter variable */
+    int multiple; 
+    int counter;
+} sharedVars;
+
+
+
+int main(){
+    int shmid = shmget(IPC_PRIVATE, sizeof(sharedVars), IPC_CREAT | 0666); /* create shared memory segment */
+    if (shmid < 0) { 
+        perror("shmget failed");
+        exit(1);
+    }
+
+    sharedVars *shared = (sharedVars *) shmat(shmid, NULL, 0); /* attach shared memory segment to the parent */
+    if (shared == (sharedVars *)-1) {
+        perror("shmat failed");
+        exit(1);
+    }
+
+    shared->multiple = 3; /* initialize the shared multiple */
+    shared->counter = 0; /* initialize the shared counter */
+
+    pid_t pid; 
+    
+    pid = fork(); /* fork the child process */
+
+    if (pid < 0) { /* error occurred */
+        fprintf(stderr, "Fork Failed\n");
+        shmctl(shmid, IPC_RMID, NULL); /* removing the shared memory segment */
+        return 1;
+    }
+    else if (pid == 0){
+        char shm_id_str[16];
+        sprintf(shm_id_str, "%d", shmid); /* pass shmid as argument */
+        execlp("./question4_p2", "question4_p2", shm_id_str, NULL); /* using exec function to launch process 2 */
+    }
+    else{
+        while (shared->counter <= 500){
+            if(shared->counter % shared->multiple == 0){ /* count is a multiple of 3 */
+                printf("_Process1_ || Cycle number: %d is a multiple of %d\n", shared->counter, shared->multiple);
+                shared->counter++;
+            }
+            else{
+                printf("_Process1_ || Cycle number: %d\n", shared->counter); /* count is not a multiple of 3 */
+            }
+            shared->counter++;
+            usleep(25000); /* delay 0.25s */
+        }
+        shmdt(shared); /* detach shared memory segment from parent */
+        shmctl(shmid, IPC_RMID, NULL); /* removing the shared memory segment */
+
+        wait(NULL); /* wait for child process to complete */
+        printf("Shared counter > 500. Process 1 and Process 2 finished.\n");
+    }
+    return 0;
+}
---
+++ question4_p2.c
@@ -0,0 +1,53 @@
+/*
+* QUESTION 4 - Sharing memory between parent and child processes
+* Using the memory mapping functions to share memory between a parent and child process.
+*
+* question4_p2.c: Executed by question4_p1.c using exec() and accesses shared memory
+* to print cycle numbers in increasing order until 500 is reached.
+*/
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/mman.h> /* for memory mapping functions */
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+
+typedef struct {
+    int multiple; 
+    int counter;
+} sharedVars;
+
+int main(int argc, char *argv[]){
+    if (argc < 2){
+        fprintf(stderr, "Missing Shared Memory ID Argument\n");
+        return 1;
+    }
+
+    int shmid = atoi(argv[1]); /* get shared memory id from argument */
+
+    sharedVars *shared = (sharedVars *) shmat(shmid, NULL, 0); /* attach shared memory segment to the parent */
+    if (shared == (sharedVars *)-1) {
+        perror("shmat failed");
+        exit(1);
+    }
+
+    while (shared->counter <= 100){
+        usleep(15000); /* a delay for 0.015s */
+    }
+
+    while (shared->counter <= 500){
+        if (shared->counter % shared->multiple ==0){
+            printf("_Process2_ || Cycle number: %d is a multiple of %d\n", shared->counter, shared->multiple);
+        }
+        else{
+            printf("_Process2_ || Cycle number: %d\n", shared->counter); /* count is not a multiple of 3 */
+        }
+        usleep(25000); /* a delay for 0.05s */
+    }
+    printf("Shared counter > 500. Process 2 finished.\n");
+    shmdt(shared); /* detach shared memory segment */
+    return 0;
+}
