877ba341af5d114711c964d9a63a0f787e22c1ea
akshavibaskaran <akshavi.bt@gmail.com>
2025-11-06 18:04:07 -0500

    Question 1 completion

---
+++ question1.c
@@ -0,0 +1,39 @@
+/*
+* QUESTION 1 - Concurrent Processes in Unix
+* Running Two Processes: Parent and Child 
+* 
+* question1.c: This program creates a child process using fork system call
+*/ 
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+    pid_t pid; 
+
+    /* fork the child process */
+    pid = fork();
+
+    if (pid < 0) { /* error occurred */
+        fprintf(stderr, "Fork Failed");
+        return 1;
+    }
+    else if (pid == 0) { /* child process */
+        int count1 = 0;
+        while(1){
+            count1++;
+            printf("Child PID: %d , Count: #%d\n", getpid(), count1);
+            usleep(500000); /* a delay for 0.5s */
+        }
+    }
+    else {
+        int count2 = 0; /* parent process */
+        while(1){
+            count2++;
+            printf("Parent PID: %d , Count: #%d\n", getpid(), count2); // ask if count should begin at 1 or if it can start at the huge number
+            usleep(500000); /* a delay for 0.5s */
+        }
+    }
+    return 0;
+}
